6c601ccdd0a1d11bf7eebad792cdad42e0a9f16c
Malhotra Computer <monstergamingbihar01@gmail.com>
2026-02-23 14:11:26 +0530

    Update and rename p9.py to p0009.py

---
+++ Python Programming Practice/Day1 to Day 5/p0009.py
@@ -0,0 +1,4 @@
+name = "Vicky"
+age = 25
+city = "Delhi"
+print("MY Name is ",name,"\nMY Age is ",age,"\nCity", city)
--- Python Programming Practice/Day1 to Day 5/p9.py
+++
@@ -1,4 +0,0 @@
-name = "Vicky"
-age = 25
-city = "Delhi"
-print("MY Name is ",name,"\nMY Age is ",age,"\nCity", city)
